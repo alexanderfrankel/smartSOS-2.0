51ffc185e2b4bbc0cb4bcfde08b9992c6d32c3ff
Alexander Frankel <alexxander.frankel@gmail.com>
2014-07-16 15:12:04 -0400

    Add new item funtionality for item not in amazon

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,11 @@
 class ItemsController < ApplicationController
 	def create
-		@item = Item.new(name: params[:name]
+		@item = Item.new(name: params[:item])
 
 		if @item.save
+			redirect_to new_campaign_request_path(params[:campaign_id])
 		else
+			puts @item.errors
 			flash.now.alert = "Apologies. Your item was not created."
 		end
 	end
