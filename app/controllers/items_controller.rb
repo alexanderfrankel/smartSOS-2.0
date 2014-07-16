0d97da808dd7d60b3d6160ecf782426385fd4c52
Alexander Frankel <alexxander.frankel@gmail.com>
2014-07-16 14:28:22 -0400

    Create Items controller

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,10 @@
+class ItemsController < ApplicationController
+	def create
+		@item = Item.new(name: params[:name]
+
+		if @item.save
+		else
+			flash.now.alert = "Apologies. Your item was not created.
+		end
+	end
+end
