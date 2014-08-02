8b622c49c63890aec1023801641b94414167896c
Jim Gay <jim@saturnflyer.com>
2014-08-02 11:58:47 -0400

    split out rendering a pledged item from iteration

--- app/assets/javascripts/pledges/views/requested_items_view.js
+++ app/assets/javascripts/pledges/views/requested_items_view.js
@@ -10,22 +10,26 @@ RequestedItemsView.prototype = {
         $('.requested-items-list').empty();
 
         for (var i = 0; i < requestedItems.length; i++) {
-            var requestedItemFormat = '<div class="requested-item" data-id="';
-            requestedItemFormat += requestedItems[i].id;
-            requestedItemFormat += '">';
-            requestedItemFormat += '<div class="requested-item-img">'
-            requestedItemFormat += '<img src="' + requestedItems[i].url + '">'
-            requestedItemFormat += '</div>'
-            requestedItemFormat += '<div class="requested-item-name">';
-            requestedItemFormat += requestedItems[i].name;
-            requestedItemFormat += '</div>';
-            requestedItemFormat += '<div class="requested-item-quantity"> Quantity: x';
-            requestedItemFormat += requestedItems[i].quantity;
-            requestedItemFormat += '</div>';
-            requestedItemFormat += '<div class="requested-item-price">Price: $';
-            requestedItemFormat += requestedItems[i].price;
-            requestedItemFormat += '</div></div>';
-            $('.requested-items-list').append(requestedItemFormat);
+            $('.requested-items-list').append(this.render_item(requestedItems[i]));
         }
+    },
+
+    render_item: function(item){
+      var requestedItemFormat = '<div class="requested-item" data-id="';
+      requestedItemFormat += item.id;
+      requestedItemFormat += '">';
+      requestedItemFormat += '<div class="requested-item-img">'
+      requestedItemFormat += '<img src="' + item.url + '">'
+      requestedItemFormat += '</div>'
+      requestedItemFormat += '<div class="requested-item-name">';
+      requestedItemFormat += item.name;
+      requestedItemFormat += '</div>';
+      requestedItemFormat += '<div class="requested-item-quantity"> Quantity: x';
+      requestedItemFormat += item.quantity;
+      requestedItemFormat += '</div>';
+      requestedItemFormat += '<div class="requested-item-price">Price: $';
+      requestedItemFormat += item.price;
+      requestedItemFormat += '</div></div>';
+      return requestedItemFormat;
     }
 }
