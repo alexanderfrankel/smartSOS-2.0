daef7a86b08ae1b2cf6d48ccb60543b87b88add9
Jim Gay <jim@saturnflyer.com>
2014-08-03 12:13:27 -0400

    clean up PledgedItemsList reduceQuantity to use the List item function

--- app/assets/javascripts/pledges/models/pledged_items_list.js
+++ app/assets/javascripts/pledges/models/pledged_items_list.js
@@ -8,23 +8,25 @@ function PledgedItemsList(registry) {
 
 PledgedItemsList.prototype = new List();
 
-PledgedItemsList.prototype.reduceQuantity = function(pledge_id) {
-      for ( var i = 0; i < this.items.length; i++) {
-        if(this.items[i].id === pledge_id) {
-          this.items[i].quantity--;
-          if(this.items[i].quantity === 0) {
-            this.items.splice(i, 1)
-          }
-        }
-      }
-      this.updateTotal();
+PledgedItemsList.prototype.reduceQuantity = function(item_id) {
+  var item = this.item(item_id);
+  if(item && item.quantity > 0) {
+    item.quantity--;
+
+    if(item.quantity === 0) {
+      this.items.splice(this.items.indexOf(item), 1)
     }
+    this.updateTotal();
+    return true
+  }
+  return false
+}
 
 PledgedItemsList.prototype.data = function(){
-      var formattedPledgeData = []
-      for(var i=0; i < this.items.length; i++) {
-        var pledge = this.items[i];
-        formattedPledgeData.push({"request_id": pledge.id, "quantity": pledge.quantity});
-      }
-      return {"pledge": formattedPledgeData};
-    }
+  var formattedPledgeData = []
+  for(var i=0; i < this.items.length; i++) {
+    var pledge = this.items[i];
+    formattedPledgeData.push({"request_id": pledge.id, "quantity": pledge.quantity});
+  }
+  return {"pledge": formattedPledgeData};
+}
